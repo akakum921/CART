f0a5661043ea66f226ee716331957ed8fddfdd2e
AKASH KUMAR SINGH <as5413@srmist.edu.in>
2022-02-10 23:47:18 +0530

    Added props to cartitem

--- src/Cart.js
+++ src/Cart.js
@@ -2,13 +2,45 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: "Mobile Phone",
+                    qty: 1,
+                    img: '',
+                    id: 1
+                  },
+                  {
+                    price: 500,
+                    title: "Watch",
+                    qty: 15,
+                    img: '',
+                    id: 2,
+                  },
+                  {
+                    price: 600,
+                    title: "Television",
+                    qty: 4,
+                    img: '',
+                    id: 3
+                  }
+            ]
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+      }
   render () {
+      const { products } = this.state;
       return (
         <div className="cart">
-          <CartItem/>
-          <CartItem/>
-          <CartItem/>
-
+          {products.map((product) => (
+             <CartItem 
+              product = {product} 
+              key = {product.id}
+              />
+          ))}
         </div>
       );
   }
